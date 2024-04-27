6eeae5f7d39505441642114773c0b53d5d4dbf8a
omognuni <melbong0069@gmail.com>
2024-04-27 11:55:32 +0900

    [Feat] profile image

--- app/account/serializers/profile.py
+++ app/account/serializers/profile.py
@@ -1,8 +1,17 @@
-from account.models import Profile
+from account.models import Profile, ProfileImage
 from rest_framework import serializers
 
 
+class ProfileImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProfileImage
+        fields = ("id", "image")
+        read_only_fields = ("id",)
+
+
 class ProfileSerializer(serializers.ModelSerializer):
+    images = ProfileImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Profile
@@ -16,6 +25,25 @@ class ProfileSerializer(serializers.ModelSerializer):
             "religion",
             "is_smoke",
             "drinking_frequency",
-            "image",
+            "images",
         )
         read_only_fields = ("id", "user")
+
+
+class ProfilePostSerializer(serializers.ModelSerializer):
+    images = serializers.ListField(
+        child=serializers.ImageField(), write_only=True, required=False
+    )
+
+    class Meta:
+        model = Profile
+        fields = (
+            "nickname",
+            "height",
+            "job",
+            "residence",
+            "religion",
+            "is_smoke",
+            "drinking_frequency",
+            "images",
+        )
--- app/account/views/profile.py
+++ app/account/views/profile.py
@@ -1,4 +1,4 @@
-from account.serializers.profile import ProfileSerializer
+from account.serializers.profile import ProfilePostSerializer, ProfileSerializer
 from account.services.profile import ProfileService
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
@@ -22,17 +22,17 @@ class ProfileViewset(viewsets.GenericViewSet):
         return Response(status=status.HTTP_200_OK, data=output_serializer.data)
 
     def update(self, request):
-        input_serializer = ProfileSerializer(request.data)
+        input_serializer = ProfileSerializer(data=request.data)
         input_serializer.is_valid(raise_exception=True)
 
         service = ProfileService(user=request.user)
         profile = service.update_my_profile(input_serializer.validated_data)
 
-        output_serializer = ProfileSerializer(profile)
+        output_serializer = ProfilePostSerializer(profile)
         return Response(status=status.HTTP_200_OK, data=output_serializer.data)
 
     def create(self, request):
-        input_serializer = ProfileSerializer(request.data)
+        input_serializer = ProfilePostSerializer(data=request.data)
         input_serializer.is_valid(raise_exception=True)
 
         service = ProfileService(user=request.user)
