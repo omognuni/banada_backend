6b2092a3264b6d57f1c0fbd5863e7dbbbee1a7e8
omognuni <melbong0069@gmail.com>
2024-04-27 12:09:48 +0900

    [Feat] swagger

--- app/account/views/profile.py
+++ app/account/views/profile.py
@@ -1,14 +1,25 @@
 from account.serializers.profile import ProfilePostSerializer, ProfileSerializer
 from account.services.profile import ProfileService
+from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description="요청을 보낸 유저의 프로필(마이페이지 - 나의 프로필)을 조회"
+    ),
+    retreive=extend_schema(description="다른 유저의 프로필을 조회"),
+    create=extend_schema(request=ProfilePostSerializer),
+)
 class ProfileViewset(viewsets.GenericViewSet):
     serializer_class = ProfileSerializer
 
     def list(self, request):
+        """
+        요청을 보낸 유저의 프로필(마이페이지 - 나의 프로필)을 조회
+        """
         service = ProfileService(user=request.user)
         profile = service.fetch_my_profile()
         output_serializer = ProfileSerializer(profile)
@@ -16,6 +27,9 @@ class ProfileViewset(viewsets.GenericViewSet):
         return Response(status=status.HTTP_200_OK, data=output_serializer.data)
 
     def retrieve(self, request, id):
+        """
+        다른 유저의 프로필을 조회
+        """
         service = ProfileService(user=request.user)
         profile = service.fetch_profile(id)
         output_serializer = ProfileSerializer(profile)
