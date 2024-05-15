a4971e8b3bfae8f4e94e01db6c3e56cc4d87ce9c
omognuni <melbong0069@gmail.com>
2024-05-15 19:08:59 +0900

    swagger

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,6 +13,11 @@ urlpatterns = [
         "api/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
     path(
         "api/v1/",
