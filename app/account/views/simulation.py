ce26f96b9e7c5cf5a1e1d344a9ed43e6e3372b91
omognuni <melbong0069@gmail.com>
2024-05-15 16:28:32 +0900

    [Feat] tags simulation

--- app/account/views/simulation.py
+++ app/account/views/simulation.py
@@ -4,6 +4,7 @@ from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema
 from rest_framework import viewsets
 
 
+@extend_schema(tags=["Simulation"])
 class SimulationViewSet(viewsets.ModelViewSet):
     queryset = Simulation.objects.all()
     serializer_class = SimulationSerializer
