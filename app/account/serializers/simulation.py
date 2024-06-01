02bff5435b874865d3c6ba5c502a5ccf0cd454e3
omognuni <melbong0069@gmail.com>
2024-06-01 18:52:24 +0900

    [Feat] simulation

--- app/account/serializers/simulation.py
+++ app/account/serializers/simulation.py
@@ -1,9 +1,17 @@
-from account.models import Simulation
+from account.models import AnswerChoice, Simulation
 from rest_framework import serializers
 
 
+class SimulationAnswerChoiceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AnswerChoice
+        fields = ("id", "simulation", "index", "content")
+
+
 class SimulationSerializer(serializers.ModelSerializer):
+    answer_choices = SimulationAnswerChoiceSerializer(many=True)
 
     class Meta:
         model = Simulation
-        fields = ("id", "category", "question")
+        fields = ("id", "category", "question", "answer_choices")
