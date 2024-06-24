c604d3536a55cfcabd7081994271d3ba166105ba
omognuni <melbong0069@gmail.com>
2024-06-24 12:40:00 +0900

    [Fix] settings

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -24,3 +26,8 @@ urlpatterns = [
         include([path("", include("account.urls")), path("", include("contact.urls"))]),
     ),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
