42de2610f09ab053eaed92cd7afb65f743d34718
omognuni <melbong0069@gmail.com>
2024-06-24 20:49:57 +0900

    [Fix] set charfield max_length

--- app/contact/models.py
+++ app/contact/models.py
@@ -9,15 +9,15 @@ class Contact(SoftDeletedModel, TimeStampModel):
     profile = models.ForeignKey(
         "account.Profile", related_name="contacts", on_delete=models.CASCADE
     )
-    phone_number = models.CharField(max_length=20, blank=True)
+    phone_number = models.CharField(max_length=200, blank=True)
 
 
 class SNSInfo(SoftDeletedModel, TimeStampModel):
     contact = models.ForeignKey(
         "contact.Contact", related_name="snsinfos", on_delete=models.CASCADE, null=True
     )
-    service = models.CharField(max_length=20, choices=SNSService.choices(), blank=True)
-    username = models.CharField(max_length=20, blank=True)
+    service = models.CharField(max_length=200, choices=SNSService.choices(), blank=True)
+    username = models.CharField(max_length=200, blank=True)
 
 
 class Message(SoftDeletedModel, TimeStampModel):
@@ -35,7 +35,7 @@ class Message(SoftDeletedModel, TimeStampModel):
     )
     content = models.TextField(blank=True)
     status = models.CharField(
-        choices=MessageStatus.choices(), max_length=20, default=MessageStatus.WAIT
+        choices=MessageStatus.choices(), max_length=200, default=MessageStatus.WAIT
     )
 
     @property
@@ -52,6 +52,6 @@ class Message(SoftDeletedModel, TimeStampModel):
 
 class MessageType(models.Model):
     name = models.CharField(
-        choices=MessageTypeChoices.choices(), max_length=20, blank=True
+        choices=MessageTypeChoices.choices(), max_length=200, blank=True
     )
     cost = models.IntegerField(blank=True, default=0)
