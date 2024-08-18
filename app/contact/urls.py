2505d5354faf6b942345fa5e6b0ea86c7d940448
omognuni <melbong0069@gmail.com>
2024-08-18 12:08:39 +0900

    [Fix] 수정사항

--- app/contact/urls.py
+++ app/contact/urls.py
@@ -1,3 +1,4 @@
+from contact.views.contact import ContactViewSet
 from contact.views.message import MessageViewSet
 from contact.views.message_type import MessageTypeViewSet
 from django.urls import include, path
@@ -6,6 +7,7 @@ from rest_framework.routers import SimpleRouter
 app_name = "contact"
 
 router = SimpleRouter()
+router.register(r"contact", ContactViewSet, "contact")
 router.register(r"message", MessageViewSet, "message")
 router.register(r"messagetype", MessageTypeViewSet, "messagetype")
 
