cc0626665cfce620e86d10eb0f81d8e50a1016c1
omognuni <melbong0069@gmail.com>
2024-08-25 11:45:14 +0900

    [Fix] validation endpoint 분리, image 엔드포인트 추가

--- app/profile/urls.py
+++ app/profile/urls.py
@@ -1,3 +1,4 @@
+from profile.views.image import ImageViewSet
 from profile.views.profile import ProfileViewSet
 from profile.views.simulation import SimulationViewSet
 from profile.views.user import UserViewSet
@@ -9,6 +10,7 @@ app_name = "profile"
 
 router = SimpleRouter()
 router.register("profiles", ProfileViewSet, "profile")
+router.register("images", ProfileViewSet, "image")
 router.register("simulations", SimulationViewSet, "simulation")
 router.register("users", UserViewSet, "user")
 
