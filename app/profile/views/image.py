9978835ea5174d84c81c2273e6535aaf155b71ba
omognuni <melbong0069@gmail.com>
2024-08-25 11:50:12 +0900

    swagger

--- app/profile/views/image.py
+++ app/profile/views/image.py
@@ -1,9 +1,11 @@
 from profile.models import ProfileImage
 from profile.serializers.profile import ProfileImageSerializer
 
+from drf_spectacular.utils import extend_schema
 from rest_framework.viewsets import ModelViewSet
 
 
+@extend_schema(tags=["ProfileImage"])
 class ImageViewSet(ModelViewSet):
     queryset = ProfileImage.objects.all()
     serializer_class = ProfileImageSerializer
