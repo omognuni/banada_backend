3cfb7fbae1a9625785886d5b2a071d22e2533f19
omognuni <melbong0069@gmail.com>
2024-09-08 14:19:28 +0900

    [Test] past match data

---
+++ app/core/management/commands/create_past_match.py
@@ -0,0 +1,43 @@
+from profile.enums import CategoryChoices, GenderChoices
+from profile.models import (
+    AnswerChoice,
+    Profile,
+    ProfileAnswer,
+    ProfileImage,
+    Simulation,
+)
+
+import django
+from contact.enums import MessageStatus, MessageTypeChoices, SNSService
+from contact.models import Contact, Message, MessageType, SNSInfo
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ImproperlyConfigured
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+fake = Faker()
+
+
+class Command(BaseCommand):
+    def create_message(self, sender, receiver, message_type):
+        message = Message.objects.create(
+            sender=sender,
+            receiver=receiver,
+            message_type=message_type,
+            content=fake.text(),
+            status=fake.random_element(
+                elements=[MessageStatus.EXPIRED, MessageStatus.REFUSED]
+            ),
+        )
+        return message
+
+    def handle(self, *args, **options):
+        users = get_user_model().objects.all()
+
+        for user in users:
+            profile = Profile.objects.filter(user=user)
+
+            for _ in range(100):
+                receiver = Profile.objects.order_by("?").first()
+                self.create_message(sender=profile, receiver=receiver)
+                self.create_message(sender=receiver, receiver=profile)
