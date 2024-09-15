67e1e1512182d3e0fd725d578f1a32a4eaf8426f
omognuni <melbong0069@gmail.com>
2024-09-15 20:39:47 +0900

    [Feat] login callback

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from user.views import SocialCallbackView
+from user.views import KakaoLogin, SocialCallbackView
 
 urlpatterns = [
     path(
         "kakao/login/callback/",
         SocialCallbackView.as_view(),
         name="kakao_callback",
-    )
+    ),
+    path("kakao/login/", KakaoLogin.as_view(), name="kakao_login"),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,8 @@
+import requests
 from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from django.urls import reverse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -18,20 +20,8 @@ class SocialCallbackView(APIView):
         access_token = request.COOKIES.get("refresh_token") or request.COOKIES.get(
             "access"
         )
-        # POST 데이터에 refresh_token을 "access_token"이라는 이름으로 추가합니다.
-        # request.POST는 QueryDict로 불변 객체이기 때문에 복사하여 수정해야 합니다.
-        data = request.POST.copy()
-        data["code"] = code
-        data["state"] = state
-        data["access_token"] = access_token
 
-        # 새로운 QueryDict를 사용하여 request._request.POST을 덮어씁니다.
-        request._request.POST = data
-
-        # KakaoLogin 같은 SocialLoginView를 호출하여 사용자 로그인 및 JWT 발급
-        view = KakaoLogin.as_view()
-        response = view(
-            request._request
-        )  # `_request`를 사용하여 원본 WSGIRequest를 전달
-
+        url = reverse("kakao_login")
+        data = {"code": code, "state": state, "access_token": access_token}
+        response = requests.post(url, data=data)
         return response
