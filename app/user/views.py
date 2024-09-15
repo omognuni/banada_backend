08d80360ed659089cb45672dc6aa22539de02b7a
omognuni <melbong0069@gmail.com>
2024-09-15 22:42:06 +0900

    [Fix] callback

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,10 @@
 from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from django.urls import reverse
 
 
 class KakaoLogin(SocialLoginView):
     adapter_class = KakaoOAuth2Adapter
+    callback_url = "https://banada.duckdns.org/auth/login/"
     client_class = OAuth2Client
