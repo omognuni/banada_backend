ad072c90f59fde460c0d246099e1b48ed2312ee9
omognuni <melbong0069@gmail.com>
2024-09-15 15:11:00 +0900

    auth settings

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -169,6 +169,16 @@ SOCIALACCOUNT_PROVIDERS = {
     },
 }
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
+
 SPECTACULAR_SETTINGS = {
     # General schema metadata. Refer to spec for valid inputs
     # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.3.md#openapi-object
--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,33 @@
-from allauth.socialaccount.providers.instagram.views import InstagramOAuth2Adapter
+from allauth.socialaccount.models import SocialToken
 from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from dj_rest_auth.registration.views import SocialLoginView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-class KakaoLogin(SocialLoginView):
-    adapter_class = KakaoOAuth2Adapter
-    # callback_url = "https://banada.duckdns.org/accounts/kakao/login/callback/"
-    # client_class = OAuth2Client
+class KakaoLoginView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        access_token = request.data.get("access_token")
+
+        if access_token:
+            # KakaoOAuth2Adapter를 사용하여 토큰 검증 및 사용자 정보 가져오기
+            adapter = KakaoOAuth2Adapter()
+            client = OAuth2Client(
+                request, client_id=adapter.get_provider().get_app(request).client_id
+            )
+            token = SocialToken(token=access_token)
+
+            # 사용자가 존재하는지 확인하고, 없으면 생성
+            login = adapter.complete_login(
+                request, app=adapter.get_provider().get_app(request), token=token
+            )
+            login.token = token
+            login.user.save()
+
+            # 여기서 추가적으로 user 정보나 다른 응답 데이터를 구성할 수 있습니다.
+            return Response({"access_token": access_token, "user": login.user.id})
+        else:
+            return Response({"error": "Access token not provided"}, status=400)
