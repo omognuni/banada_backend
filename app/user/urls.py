212fb10d7f9a3eb095f3d34e7be2538d1efb9ec0
omognuni <melbong0069@gmail.com>
2024-09-15 22:20:55 +0900

    [Feat] kakao token

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,11 +1,5 @@
 from allauth.socialaccount.providers.oauth2.views import OAuth2CallbackView
 from django.urls import path
-from user.views import CustomKakaoOAuth2Adapter
+from user.views import KakaoLogin
 
-urlpatterns = [
-    path(
-        "kakao/login/callback/",
-        OAuth2CallbackView.adapter_view(CustomKakaoOAuth2Adapter),
-        name="kakao_callback",
-    )
-]
+urlpatterns = [path("auth/kakao/", KakaoLogin.as_view(), name="kakao_auth")]
--- app/user/views.py
+++ app/user/views.py
@@ -1,14 +1,8 @@
-import requests
 from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 
-class CustomKakaoOAuth2Adapter(KakaoOAuth2Adapter):
-    def complete_login(self, request, app, token, **kwargs):
-
-        return super().complete_login(request, app, token, **kwargs)
+class KakaoLogin(SocialLoginView):
+    adapter_class = KakaoOAuth2Adapter
+    client_class = OAuth2Client
