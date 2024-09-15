e50c26107f1f4e6d8fc7de4ad8b54868aadfa1f3
omognuni <melbong0069@gmail.com>
2024-09-15 15:53:23 +0900

    [Feat] login callback

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,3 +1,11 @@
+from allauth.socialaccount.providers.oauth2.views import OAuth2CallbackView
 from django.urls import path
+from user.views import CustomKakaoOAuth2Adapter
 
-urlpatterns = [path("kakao/login/callback/")]
+urlpatterns = [
+    path(
+        "kakao/login/callback/",
+        OAuth2CallbackView.adapter_view(CustomKakaoOAuth2Adapter),
+        name="kakao_callback",
+    )
+]
