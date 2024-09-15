f40ea9f7ab5d6682724404797beca8aab3033e1a
omognuni <melbong0069@gmail.com>
2024-09-15 21:23:56 +0900

    [Feat] login callback

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path
-from user.views import KakaoLogin
 
-urlpatterns = [
-    # path(
-    #     "kakao/login/callback/",
-    #     SocialCallbackView.as_view(),
-    #     name="kakao_callback",
-    # ),
-    path("kakao/login/", KakaoLogin.as_view(), name="kakao_login"),
-]
+# urlpatterns = [
+
+# ]
