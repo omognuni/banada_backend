2e63541d6e9eba7acbbabf780f39949bc38fa6ba
omognuni <melbong0069@gmail.com>
2024-09-15 20:30:31 +0900

    [Feat] login callback

--- app/user/views.py
+++ app/user/views.py
@@ -14,15 +14,19 @@ class SocialCallbackView(APIView):
     def get(self, request, *args, **kwargs):
         # 클라이언트로부터 소셜 제공자의 액세스 토큰을 쿼리 매개변수로 받습니다.
         code = request.query_params.get("code")
+        state = request.query_params.get("state")
         access_token = request.COOKIES.get("refresh_token") or request.COOKIES.get(
             "access"
         )
+        # POST 데이터에 refresh_token을 "access_token"이라는 이름으로 추가합니다.
+        # request.POST는 QueryDict로 불변 객체이기 때문에 복사하여 수정해야 합니다.
+        data = request.POST.copy()
+        data["code"] = code
+        data["state"] = state
+        data["access_token"] = access_token
 
-        query_params = request.GET.copy()
-        query_params["access_token"] = access_token
-
-        # 새로운 QueryDict를 사용하여 request._request.GET을 덮어씁니다.
-        request._request.GET = query_params
+        # 새로운 QueryDict를 사용하여 request._request.POST을 덮어씁니다.
+        request._request.POST = data
 
         # KakaoLogin 같은 SocialLoginView를 호출하여 사용자 로그인 및 JWT 발급
         view = KakaoLogin.as_view()
