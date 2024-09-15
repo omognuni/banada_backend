d51ac6e6499347df5af2cb09dc769077691cec9f
omognuni <melbong0069@gmail.com>
2024-09-15 18:17:54 +0900

    [Feat] login callback

--- app/user/views.py
+++ app/user/views.py
@@ -13,13 +13,16 @@ class KakaoLogin(SocialLoginView):
 class SocialCallbackView(APIView):
     def get(self, request, *args, **kwargs):
         # 클라이언트로부터 소셜 제공자의 액세스 토큰을 쿼리 매개변수로 받습니다.
-        access_token = request.query_params.get("access_token")
+        code = request.query_params.get("code")
+        access_token = request.COOKIES.get("refresh_token") or request.COOKIES.get(
+            "access"
+        )
 
-        if not access_token:
-            return Response(
-                {"error": "Access token is required", "msg": f"{request.query_params}"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        query_params = request.GET.copy()
+        query_params["access_token"] = access_token
+
+        # 새로운 QueryDict를 사용하여 request._request.GET을 덮어씁니다.
+        request._request.GET = query_params
 
         # KakaoLogin 같은 SocialLoginView를 호출하여 사용자 로그인 및 JWT 발급
         view = KakaoLogin.as_view()
