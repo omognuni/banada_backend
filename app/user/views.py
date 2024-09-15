8f7d6144a9a0f64f3dc0e650eabd8bada470d45b
omognuni <melbong0069@gmail.com>
2024-09-15 13:42:07 +0900

    [Feat] kakao login urls

--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,10 @@
 from allauth.socialaccount.providers.instagram.views import InstagramOAuth2Adapter
 from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from dj_rest_auth.registration.views import SocialConnectView
+from dj_rest_auth.registration.views import SocialLoginView
 
 
-class KakaoConnect(SocialConnectView):
+class KakaoConnect(SocialLoginView):
     adapter_class = KakaoOAuth2Adapter
     callback_url = "https://banada.duckdns.org/accounts/kakao/login/callback/"
     client_class = OAuth2Client
