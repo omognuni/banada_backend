5b8f9c443cc96389821a8d2f8f860b567af47b66
omognuni <melbong0069@gmail.com>
2024-09-15 17:56:50 +0900

    [Feat] login callback

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,11 +1,11 @@
 from allauth.socialaccount.providers.oauth2.views import OAuth2CallbackView
 from django.urls import path
-from user.views import CustomKakaoOAuth2Adapter
+from user.views import SocialCallbackView
 
 urlpatterns = [
     path(
         "kakao/login/callback/",
-        OAuth2CallbackView.adapter_view(CustomKakaoOAuth2Adapter),
+        SocialCallbackView.as_view(),
         name="kakao_callback",
     )
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,12 +1,30 @@
-from allauth.socialaccount.helpers import complete_social_login
-from allauth.socialaccount.models import SocialAccount
 from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
-from django.http import HttpResponse
-from django.urls import reverse
-from django.utils.http import urlencode
-from rest_framework_simplejwt.tokens import RefreshToken
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-class CustomKakaoOAuth2Adapter(KakaoOAuth2Adapter):
-    def complete_login(self, request, app, token, **kwargs):
-        return super().complete_login(request, app, token, **kwargs)
+class KakaoLogin(SocialLoginView):
+    adaptor_class = KakaoOAuth2Adapter
+
+
+class SocialCallbackView(APIView):
+    def post(self, request, *args, **kwargs):
+        # 클라이언트로부터 소셜 제공자의 액세스 토큰을 받습니다.
+        access_token = request.data.get("access_token")
+
+        if not access_token:
+            return Response(
+                {"error": "Access token is required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # GoogleLogin 같은 SocialLoginView를 호출하여 사용자 로그인 및 JWT 발급
+        view = KakaoLogin.as_view()
+        response = view(
+            request._request
+        )  # `_request`를 사용하여 원본 WSGIRequest를 전달
+
+        return response
