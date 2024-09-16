31fe823ffea50a3a032bec89ceb2430a9373aa57
omognuni <melbong0069@gmail.com>
2024-09-16 17:08:21 +0900

    [Fix] callback

--- app/user/views.py
+++ app/user/views.py
@@ -12,6 +12,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
 from django.http import JsonResponse
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -60,6 +61,7 @@ def kakao_callback(request):
         headers={"Authorization": f"Bearer {access_token}"},
     )
     profile_json = profile_request.json()
+    uid = KakaoProvider.extract_uid(profile_json)
     kakao_account = profile_json.get("kakao_account")["profile"]
     nickname = kakao_account.get("nickname")
 
@@ -68,18 +70,23 @@ def kakao_callback(request):
     """
     try:
         user = get_user_model().objects.get(username=nickname)
-    except get_user_model().DoesNotExist:
-        adapter = get_adapter(request)
-        user = adapter.new_user(request)
-        user.username = nickname
-        user.set_unusable_password()
-        adapter.save_user(request, user)
+        social_user = SocialAccount.objects.get(user=user)
+        if social_user is None:
+            return JsonResponse(
+                {"err_msg": "email exists but not social user"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if social_user.provider != "kakao":
+            return JsonResponse(
+                {"err_msg": "no matching social type"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-        # 소셜 계정 생성
-        social_account = SocialAccount(
-            user=user, provider=KakaoProvider.id, uid=profile_json.get("id")
-        )
-        social_account.save()
+    except get_user_model().DoesNotExist:
+        user = get_user_model().objects.create_user(username=nickname)
+        user.set_password()
+        user.save()
+        social_user = SocialAccount.objects.create(user=user, uid=uid, provider="kakao")
 
     # JWT 생성
     refresh = RefreshToken.for_user(user)
