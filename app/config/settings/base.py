62ac1dcf6ffd22eb17ba9afe6243f8667115a2c8
omognuni <melbong0069@gmail.com>
2024-10-03 14:04:37 +0900

    set auth true, debug false

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -121,6 +121,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
         "rest_framework.authentication.SessionAuthentication",
