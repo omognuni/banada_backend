16c58e1a83c55369f52b66fbf5ca717888514991
yongwun <yongwun@adenasoft.com>
2024-10-04 16:50:11 +0900

    [Fix] admin action

--- app/profile/admin.py
+++ app/profile/admin.py
@@ -3,6 +3,8 @@ from profile.models import Profile
 from django.contrib import admin
 
 
-@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ("nickname", "gender", "age", "created_time", "email")
+
+
+admin.site.register(Profile, ProfileAdmin)
